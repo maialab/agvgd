0616fb1f0538e2366d9c570dc9fcdf02759f161f
Ramiro Magno <ramiro.magno@gmail.com>
2022-02-02 00:38:46 +0000

    Added doc for `dev()`

--- R/dev.R
+++ R/dev.R
@@ -1,3 +1,54 @@
+#' Deviation function
+#'
+#' @description
+#' This function calculates the deviation in the sense of the Grantham deviation
+#' as introduced by Tavtigian et al. (2006). Essentially, if `x` lies within the
+#' range \[`min`, `max`\], then `dev()` returns 0. If `x` is either below `min`, or
+#' above `max`, then `dev()` returns the absolute difference between `x` and
+#' `min` or `max`, respectively.
+#'
+#' \figure{dev-def.svg}
+#'
+#' Inputs are recycled in the sense of [vctrs::vec_recycle()].
+#'
+#' @details
+#' Here's a plot showcasing `dev()` with `min = -4` and `max = 3`:
+#'
+#' ```{r dev-plot, fig.asp=2/(1+sqrt(5)), dev = 'svg'}
+#' x <- -10:10; min <- -4; max <- 3
+#' plot(x, y = dev(x, min, max), type = 'l', xlab = 'x', ylab = 'deviation')
+#' ```
+#'
+#' @param x A numeric vector.
+#' @param min A numeric vector.
+#' @param max A numeric vector.
+#'
+#' @return A numeric vector of deviations.
+#'
+#' @examples
+#' # `dev()` returns absolute differences from either min or max (whichever is
+#' # closest).
+#' dev(10, min = -4, max = 4)
+#' dev(-10, min = -4, max = 4)
+#'
+#' # `x` can be a vector
+#' dev(-10:10, min = -4, max = 4)
+#'
+#' # `min` and `max` can also be vectors, they will be recycled
+#' dev(-10:10, min = -4:16, max = 4:24)
+#'
+#' # If `x` contains `NA` values, then `dev()` will return `NA` for
+#' # those cases
+#' dev(c(10, NA), min = -4, max = 4)
+#'
+#' # For each calculation of deviation, only either `min` or `max` is used. If
+#' # the unused parameter is `NA` it won't affect the calculation:
+#' dev(c(10, 3), min = c(NA, -4), max = 4)
+#' dev(c(10, -5), min = -4, max = c(4, NA))
+#'
+#' @seealso [gd()]
+#' @md
+#' @export
 dev <- function(x, min, max) {
 
   lst <- vctrs::vec_recycle_common(x = x, min = min, max = max)
