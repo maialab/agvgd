27ca775db096b76a4be4dd4eaaf5937abbc3ca67
Ramiro Magno <ramiro.magno@gmail.com>
2022-02-02 00:36:54 +0000

    `read_substitutions` is updated; output variables are now consistent with other functions' output

--- R/read_substitutions.R
+++ R/read_substitutions.R
@@ -5,15 +5,27 @@
 #' [Align-GVGD](http://agvgd.hci.utah.edu/help.php).
 #'
 #' @param file The path to a file with amino acid substitutions.
-#' @param amino_acid_code The type of symbol used for amino acids in the returned output.
+#' @param amino_acid_code The type of symbol used for amino acids in the
+#'   returned output.
 #'
-#' @return A tibble listing the amino acids substitutions.
+#' @return A [tibble][tibble::tibble-package] listing the amino acids
+#'   substitutions.
+#'
+#' @examples
+#' # "sub.txt" is an example file containing missense substitutions formatted
+#' # according # to the requirements indicated in http://agvgd.hci.utah.edu/help.php.
+#' my_file <- system.file("extdata", "sub.txt", package = "agvgd")
+#' cat(readLines(my_file), sep = "\n")
+#'
+#' read_substitutions(file = my_file)
 #'
 #' @md
-#'
 #' @importFrom rlang .data
 #' @export
-read_substitutions <- function(file = stop('`file` must be specified'), amino_acid_code = c('one_letter', 'three_letter')) {
+read_substitutions <-
+  function(file = stop('`file` must be specified'),
+           amino_acid_code = c('one_letter', 'three_letter')) {
+
 
   # Read in all lines
   lines <- readLines(con = file)
@@ -29,14 +41,15 @@ read_substitutions <- function(file = stop('`file` must be specified'), amino_ac
 
   tbl <- tibble::as_tibble(m) %>%
     dplyr::mutate(poi = as.integer(.data$poi)) %>%
-    dplyr::relocate(.data$poi, .data$from, .data$to)
+    dplyr::relocate(.data$poi, .data$from, .data$to) %>%
+    dplyr::rename(ref = .data$from, sub = .data$to)
 
-  value <- match.arg(value)
+  amino_acid_code <- match.arg(amino_acid_code)
 
-  if (identical(value, 'three_letter')) {
+  if (identical(amino_acid_code, 'three_letter')) {
     tbl <- dplyr::mutate(tbl,
-                  from = as_three_letter(.data$from),
-                  to = as_three_letter(.data$to))
+                  ref = as_three_letter(.data$ref),
+                  sub = as_three_letter(.data$sub))
   }
 
   return(tbl)
---
+++ tests/testthat/test-read_substitutions.R
@@ -0,0 +1,37 @@
+test_that("basic usage", {
+
+  my_file <- system.file("extdata", "sub.txt", package = "agvgd")
+  ex01 <- read_substitutions(file = my_file)
+  ex02 <- read_substitutions(file = my_file, amino_acid_code = 'three_letter')
+
+  expected01 <-
+    structure(
+      list(
+        poi = c(2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L,
+                2L, 2L, 2L),
+        ref = c("P", "P", "P", "P", "P", "P", "P", "P", "P", "P", "P", "P", "P",
+                "P", "P", "P", "P", "P", "P"),
+        sub = c( "S", "R", "L", "T", "A", "V", "G", "I", "F", "Y", "C", "H",
+                 "Q", "N", "K", "D", "E", "M", "W")
+      ),
+      row.names = c(NA, -19L), class = c("tbl_df", "tbl", "data.frame")
+    )
+
+  expected02 <-
+    structure(
+      list(
+        poi = c(2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L,
+                2L, 2L, 2L),
+        ref = c("Pro", "Pro", "Pro", "Pro", "Pro", "Pro", "Pro", "Pro", "Pro",
+                "Pro", "Pro", "Pro", "Pro", "Pro", "Pro", "Pro", "Pro", "Pro",
+                "Pro"),
+        sub = c("Ser",  "Arg", "Leu", "Thr", "Ala", "Val", "Gly", "Ile", "Phe",
+                "Tyr", "Cys", "His", "Gln", "Asn", "Lys", "Asp", "Glu", "Met",
+                "Trp")),
+      row.names = c(NA, -19L),
+      class = c("tbl_df", "tbl", "data.frame"))
+
+  expect_equal(ex01, expected01)
+  expect_equal(ex02, expected02)
+
+})
