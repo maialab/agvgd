91b613315c8c7b1ed337d8e86962f83a00b87845
Ramiro Magno <ramiro.magno@gmail.com>
2022-02-03 00:32:42 +0000

    Made `ref_sequence()` more lenient on the type of input

--- R/reference_sequence.R
+++ R/reference_sequence.R
@@ -1,6 +1,4 @@
 ref_sequence <- function(alignment) {
-  if(!is_align_mat_class(alignment))
-    stop('`alignment` should be an object of class align_mat.')
 
   # Alignment as simple matrix
   m <- remove_align_mat_class(alignment)
